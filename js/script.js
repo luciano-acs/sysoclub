18786bdcddb99b3659a2da980e2ce32864b8d46c
luciano-acs <luciano.acosta@alu.frt.utn.edu.ar>
2023-10-09 12:11:02 -0300

    segunda entrega

--- js/script.js
+++ js/script.js
@@ -1,40 +1,146 @@
 // DATOs GLOBALES
 let inicioSesion = false;
+let socioActual;
 
-// Datos usuario
-let usuarioAlmacenado = 'prueba';
-let contraseñaAlmacenada = '123456';
+// Usuarios
+const socios = [
+    {
+        nombre: 'Juan',
+        apellido: 'Perez',
+        dni: 40147963,
+        usuario: 'juancitoPerez',
+        contraseña: 'juanperez963',
+        cuotas: [],
+        reservas: []
+    },
+    {
+        nombre: 'Daniela',
+        apellido: 'Mendez',
+        dni: 20145698,
+        usuario: 'daniMendez',
+        contraseña: 'danimendez698',
+        cuotas: [],
+        reservas: []
+    },
+    {
+        nombre: 'Javier',
+        apellido: 'Diaz',
+        dni: 8153759,
+        usuario: 'donJavier',
+        contraseña: 'javierDiaz759',
+        cuotas: [],
+        reservas: []
+    }
+];
 
-let opcionInicial = prompt('Bienvenido al sistema de socios del club atletico CoderHouse. Para continuar elija algunas de las siguientes opciones: \n1: Iniciar Sesión\n2: Ver info\n3: Salir');
+const existeUsuarioContraseña = (user, password) =>{
+    if(socios.some(socio => {
+        return socio['usuario'] === user && socio['contraseña'] === password;
+    })){
+        return true;
+    }else{
+        return false;
+    };
+};
 
-while (opcionInicial != '3') {
+const busquedaDeSocio = (user, password) =>{
+    let socioBusqueda = socios.find(socio => socio.usuario === user && socio.contraseña === password);
+    return socioBusqueda;
+}
+
+// Class Socio
+class Socio{
+    constructor(nombre, apellido, dni, usuario, contraseña){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.cuotas = [];
+        this.reservas = [];
+    }
+
+    pagarCuotas(mes, año){
+        let cuota = {
+            mes: mes,
+            año: año,
+            fecha: new Date()
+        }
+
+        if(!this.cuotas.find(cuota => cuota.mes === mes)){
+            this.cuotas.push(cuota);
+            alert(`Cuota del periodo ${mes}/${año} abonada!`);
+        }else{
+            alert(`La cuota del periodo ${mes}/${año} ya fue abonada`);
+        }        
+    }
+
+    realizarReserva(lugar, estado){
+        let reserva = {
+            lugar: lugar,
+            estado: estado,
+            fecha: new Date()
+        }
+
+        if(!this.reservas.find(reserva => reserva.lugar === lugar || estado)){
+            this.reservas.push(reserva);
+            alert(`Reserva de ${reserva.lugar} realizada`);
+        }else{
+            alert(`Ya tiene una reserva activa`);
+        }        
+    }
+
+    consultarCuotas() {
+        const cuotasPagadas = this.cuotas.map(cuota => `${cuota.mes}/${cuota.año}`);
+        alert(`Las cuotas pagadas son: ${cuotasPagadas.join(', ')}`);
+
+        let cantidadCuotas = 12 - this.cuotas.length;
+
+        alert(`Les queda un total de ${cantidadCuotas} por abonar. La cual implica una deuda de $${cantidadCuotas*5000}`)
+    }
+
+    consultarReservas() {
+        const reservasRealizadas = this.reservas.map(reserva => `Reserva de ${reserva.lugar} realizada el día ${reserva.fecha.toLocaleDateString()}`);
+        alert(`Las realizadas son: ${reservasRealizadas.join(', ')}\n`);
+    }
+}
+
+let opcionInicial = prompt('Bienvenido al sistema de socios del club atletico CoderHouse. Para continuar elija algunas de las siguientes opciones: \n1: Iniciar Sesión\n2: Registrarse\n3: Ver info\n4: Salir');
+
+while (opcionInicial != '4') {
     switch (opcionInicial) {
         case '1':
             login();
             break;
         case '2':
+            registrarse();
+            break;
+        case '3':
             alert('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem quo nihil quam quisquam ipsa id, ab, impedit quod alias perspiciatis fugiat ratione saepe repudiandae rem illo unde laboriosam repellat hic.');
             break;
         default:
             alert('Opcion incorrecta. Intente nuevamente');
             break;
     }
-    opcionInicial = prompt('Bienvenido al sistema de socios del club atletico CoderHouse. Para continuar elija algunas de las siguientes opciones: \n1: Iniciar Sesión\n2: Ver info\n3: Salir');
+    opcionInicial = prompt('Bienvenido al sistema de socios del club atletico CoderHouse. Para continuar elija algunas de las siguientes opciones: \n1: Iniciar Sesión\n2: Registrarse\n3: Ver info\n4: Salir');
 }
 
 function login() {
     while (inicioSesion == false) {
-        let usuario = prompt('Ingrese su usuario: ');
+        let usuario = prompt('Login\nIngrese su usuario: ');
         let contraseña = prompt('Ingrese su contraseña: ');
-        if (usuario === usuarioAlmacenado && contraseña === contraseñaAlmacenada) {
+        if (existeUsuarioContraseña(usuario,contraseña)) {
             alert('Sesion iniciada con exito');
+            let socioEncontrado = busquedaDeSocio(usuario,contraseña)
+            socioActual = new Socio(socioEncontrado.nombre,socioEncontrado.apellido,socioEncontrado.dni,socioEncontrado.usuario,socioEncontrado.contraseña,socioEncontrado.cuotas,socioEncontrado.reservas);
+            console.log(socioActual);
             inicioSesion = true;
         } else {
             alert('Usuario y contraseña no validos. Intente nuevamente');
         }
     }
 
-    let opcionMenu = prompt('Bienvenido ' + usuarioAlmacenado + '. Que desea realizar: \n1: Reservas\n2: Cuotas\n3: Cerrar sesion');
+    let opcionMenu = prompt('Bienvenido ' + socioActual.nombre + '. Que desea realizar: \n1: Reservas\n2: Cuotas\n3: Cerrar sesion');
 
     while (inicioSesion) {
         switch (opcionMenu) {
@@ -52,33 +158,37 @@ function login() {
                 break;
         }
         if (inicioSesion) {
-            opcionMenu = prompt('Bienvenido ' + usuarioAlmacenado + '. Que desea realizar: \n1: Reservas\n2: Cuotas\n3: Cerrar sesion');
+            opcionMenu = prompt('Bienvenido ' + socioActual.nombre + '. Que desea realizar: \n1: Reservas\n2: Cuotas\n3: Cerrar sesion');
         }
     }
 }
 
+function registrarse(){
+    let nombre = prompt('Nombre: ');
+    let apellido = prompt('Apellido: ');
+    let dni = parseInt(prompt('DNI: '));
+    let usuario = prompt('Usuario: ');
+    let contraseña = prompt('Contraseña: ');
+
+    if(existeUsuarioContraseña(usuario,contraseña) === false){
+        const socioNuevo = new Socio(nombre, apellido, dni, usuario, contraseña);
+        socios.push(socioNuevo);
+        alert('Usuario registrado');
+    }else{
+        alert('Usuario existente proceda a realizar el login');
+    }   
+}
+
 function reservas() {
     let opcionReserva = prompt('Ingrese la opcion deseada: \n1: Reservar cancha\n2: Consultar reserva\n3: Volver atras');
-    let reserva = false;
-    let reservaCantidad = 0;
 
     while (opcionReserva != 3) {
         switch (opcionReserva) {
             case '1':
-                if (reservaCantidad < 1) {
-                    reserva = true;
-                    reservaCantidad = 1;
-                    alert('Reserva en gestion. Verifique confirmacion');
-                } else {
-                    alert('Ya realizó la reserva permitida. Consulte la misma en el menú de reservas');
-                }
+                socioActual.realizarReserva('cancha');
                 break;
             case '2':
-                if (reserva) {
-                    alert('Su reserva esta confirmada. Dirijase a administracion del club para más detalles');
-                } else {
-                    alert('No cuenta con reservas a su nombre');
-                }
+                socioActual.consultarReservas();
                 break;
             default:
                 alert('Opcion incorrecta. Intente nuevamente');
@@ -90,21 +200,16 @@ function reservas() {
 
 function cuotas() {
     let opcionCuotas = prompt('Ingrese la opcion deseada: \n1: Consultar cuotas\n2: Pagar cuotas\n3: Volver atras');
-    let cuotasCantidad = 12;
 
     while (opcionCuotas != 3) {
         switch (opcionCuotas) {
             case '1':
-                alert('La cantidad de cuotas adeudadas es de: ' + cuotasCantidad + ' lo cual implica una deuda de $' + (5000 * cuotasCantidad));
+                socioActual.consultarCuotas(socioActual);
                 break;
             case '2':
-                let cuotasAbonar = prompt('Ingrese la cantidad de cuotas a abonar: \n');
-                if ((cuotasCantidad - cuotasAbonar) < 0) {
-                    alert('No puede pagar esa cantidad. Sus cuotas adeudadas son: ' + cuotasCantidad + '\nVuelva a intentarlo');
-                } else {
-                    alert('Se abonaron: ' + cuotasAbonar + ' donde se debitaron $' + cuotasAbonar * 5000);
-                    cuotasCantidad -= cuotasAbonar;
-                }
+                let mesAbonar = prompt('Ingrese el mes a abonar: ');
+                let anioAbonar = prompt('Ingrese el año a abonar: ');
+                socioActual.pagarCuotas(mesAbonar,anioAbonar);
                 break;
             default:
                 alert('Opcion incorrecta. Intente nuevamente');
